397ecd678bcd2c300405f00acfef58d0172a9ec4
marje <marje.harms@web.de>
2022-07-09 18:55:38 +0200

    paper function works for every bubble
    - add texture for paper
    - add different material for each paper (made them colour coded to their respective bubble)
    - right now on pressing K each bubble changes to paper (and back again on another button press) cause I didn't want to have too many buttons or research how to select one and then do a respective button press for each individually (still looks fine though)
    so ye it work :)

--- Assets/BubbleToggle.cs
+++ Assets/BubbleToggle.cs
@@ -7,31 +7,73 @@ public class BubbleToggle : MonoBehaviour
 {
     public InputActionReference toggleReference = null;
 
-    public GameObject Paper;
+    public GameObject Paper1;
+    public GameObject Paper2;
+    public GameObject Paper3;
+
     public GameObject InfoBubble1;
+    public GameObject InfoBubble2;
+    public GameObject InfoBubble3;
+
+    //precautions for not looking bad
+    private float very = 1000f;
+    private float far = 1000f;
+    private float away = 1000f;
 
 
 
     private void Awake()
     {
-        toggleReference.action.started += Toggle;
+        //spawn far away so if user already presses secondary button or K it doesn't look bad
+        Paper1.transform.position = new Vector3(very, far, away);
+        Paper2.transform.position = new Vector3(very, far, away);
+        Paper3.transform.position = new Vector3(very, far, away);
+
+        InfoBubble1.transform.position = new Vector3(very, far, away);
+        InfoBubble2.transform.position = new Vector3(very, far, away);
+        InfoBubble3.transform.position = new Vector3(very, far, away);
+
+
+        toggleReference.action.started += Toggle1;
+        toggleReference.action.started += Toggle2;
+        toggleReference.action.started += Toggle3;
 
     }
 
 
     private void OnDestroy()
     {
-        toggleReference.action.started -= Toggle;
+        toggleReference.action.started -= Toggle1;
+        toggleReference.action.started -= Toggle2;
+        toggleReference.action.started -= Toggle3;
     }
 
-    private void Toggle(InputAction.CallbackContext context)
+    private void Toggle1(InputAction.CallbackContext context)
     {
         bool isActive = !InfoBubble1.activeSelf;
         InfoBubble1.SetActive(isActive);
 
-        Paper.transform.position = InfoBubble1.transform.position;
+        Paper1.transform.position = InfoBubble1.transform.position;
         //gameObject.SetActive(true);
         //InfoBubble1.SetActive(false);
         //this.transform.position = new Vector3(0, 0, 0); //not even a toggle anymore but I gave up to do it the complicated way
+    }
+
+    private void Toggle2(InputAction.CallbackContext context)
+    {
+        bool isActive = !InfoBubble2.activeSelf;
+        InfoBubble2.SetActive(isActive);
+
+        Paper2.transform.position = InfoBubble2.transform.position;
+          
+    }
+
+    private void Toggle3(InputAction.CallbackContext context)
+    {
+        bool isActive = !InfoBubble3.activeSelf;
+        InfoBubble3.SetActive(isActive);
+
+        Paper3.transform.position = InfoBubble3.transform.position;
+
     }
 }
--- Assets/_MarjeAssets/Scripts/TutorialScripts/ToggleObjectExample.cs
+++ Assets/_MarjeAssets/Scripts/TutorialScripts/ToggleObjectExample.cs
@@ -7,26 +7,61 @@ public class ToggleObjectExample : MonoBehaviour
 {
     public InputActionReference toggleReference = null;
     public GameObject Infobubble1;
-    public GameObject Paper;
-    
+    public GameObject Infobubble2;
+    public GameObject Infobubble3;
+    public GameObject Paper1;
+    public GameObject Paper2;
+    public GameObject Paper3;
 
-    
+
+    //if I don't do that & the player presses button to make infobubble/paper appear b4 the bubble was thrown to wall it doesn't look good
+    private float very = 1000f;
+    private float far = 1000f;
+    private float away = 1000f;
+
+
+
+
+
     private void Awake()
     {
-        toggleReference.action.started += Toggle;
+        toggleReference.action.started += Toggle1;
+        toggleReference.action.started += Toggle2;
+        toggleReference.action.started += Toggle3;
+
     }
 
-    
+
     private void OnDestroy()
     {
-        toggleReference.action.started -= Toggle;
+        toggleReference.action.started -= Toggle1;
+        toggleReference.action.started -= Toggle2;
+        toggleReference.action.started -= Toggle3;
     }
 
-    private void Toggle(InputAction.CallbackContext context)
+    private void Toggle1(InputAction.CallbackContext context)
     {
         //bool isActive = !gameObject.activeSelf;
         //gameObject.SetActive(isActive);
-        Infobubble1.transform.position = new Vector3(0, 0, 0); //not even a toggle anymore but I gave up to do it the complicated way
-        Paper.transform.position = new Vector3(0, 0, 0);
+        Infobubble1.transform.position = new Vector3(very, far, away); //not even a toggle anymore but I gave up to do it the complicated way
+        Paper1.transform.position = new Vector3(very, far, away);
     }
+
+    private void Toggle2(InputAction.CallbackContext context)
+    {
+        //bool isActive = !gameObject.activeSelf;
+        //gameObject.SetActive(isActive);
+        Infobubble2.transform.position = new Vector3(very, far, away); //not even a toggle anymore but I gave up to do it the complicated way
+        Paper2.transform.position = new Vector3(very, far, away);
+    }
+
+    private void Toggle3(InputAction.CallbackContext context)
+    {
+        //bool isActive = !gameObject.activeSelf;
+        //gameObject.SetActive(isActive);
+        Infobubble3.transform.position = new Vector3(very, far, away); //not even a toggle anymore but I gave up to do it the complicated way
+        Paper3.transform.position = new Vector3(very, far, away);
+    }
+
+    //could've done this much easier with tags or layers but meh I don't wanna
 }
