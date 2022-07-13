0bf3032a409ee5a79c3169027e751be74db3d46c
Torben Storch <glo_age@hotmail.com>
2022-07-13 21:43:44 +0200

    + added movement limiter to UI sticky notes
    + added new markers

--- Assets/_TorbenAssets/Scripts/StickyNotes/UIStickyNoteMovement.cs
+++ Assets/_TorbenAssets/Scripts/StickyNotes/UIStickyNoteMovement.cs
@@ -1,8 +1,15 @@
+/*-------------------------------------------------------
+Creator: Torben Storch
+Expanded Realities P6
+last change: 13-07-2022
+Topic: Script to lock movement with an simple interactor on 2 axis -> attached to Select event inside XRSimpleInteractable
+---------------------------------------------------------*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(XRSimpleInteractable))]
 public class UIStickyNoteMovement : MonoBehaviour
 {
 
@@ -10,9 +17,13 @@ public class UIStickyNoteMovement : MonoBehaviour
 	GameObject target;
 	XRSimpleInteractable simpleInteractable;
 
+	private void Start()
+	{
+		simpleInteractable = GetComponent<XRSimpleInteractable>();
+	}
 	public void StartMovement()
 	{
-		target = target = simpleInteractable.GetOldestInteractorSelecting().transform.gameObject; 
+		target = simpleInteractable.GetOldestInteractorSelecting().transform.gameObject; 
 		start = true;
 	}
 	public void StopMovement()
@@ -25,7 +36,7 @@ public class UIStickyNoteMovement : MonoBehaviour
 	{
 		if (start)
 		{
-			gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, gameObject.transform.position.z/*target.transform.position.z*/);
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, target.transform.position.y, target.transform.position.z/*target.transform.position.z*/);
 		}
 	}
 
