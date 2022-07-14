3aefa6255121263f2a463d6344e102d2b75c56d2
marje <marje.harms@web.de>
2022-07-15 00:42:28 +0200

    Bowl to collect all magic numbers
    - add bowl with mesh renderer where the bubbles can spawn into
    - bubbles are now in one line on table
    - sprites aren't flipped anymore

--- Assets/_MarjeAssets/Scripts/WallScript.cs
+++ Assets/_MarjeAssets/Scripts/WallScript.cs
@@ -22,20 +22,23 @@ public class WallScript : MonoBehaviour
     public GameObject Paper1;
     public GameObject Paper2;
     public GameObject Paper3;
+    public GameObject BallBowl;
     public float bubblePosition = -0.5f;
+    
 
-    //vectors for the Bubbles to respawn to
-    //Blubble2
-    //commented numbers were for my scene
-    private float BubbleX = /*0.307f*/ -2.67593f;
-    private float Bubble123Y = /*1.114f*/0.718f; //the same for all 3
-    private float BubbleZ = /*-0.84f*/0.8809423f;
-    //Blubble2
-    private float Bubble2X = /*-0.08f*/-2.33593f; //the same for 2 and 3 in the test scene
-    private float Bubble2Z = /*-0.5f*/1.267943f;
-    //Blubble3
-    private float Bubble3X = /*-0.08f*/-2.67593f;
-    private float Bubble3Z = /*-0.84f*/1.267943f;
+    ////vectors for the Bubbles to respawn to
+    ////Blubble2
+    ////commented numbers were for my scene
+    //private float BubbleX = /*0.307f*/ 0.6f;
+    //private float Bubble123Y = /*1.114f*/0.718f; //the same for all 3
+    //private float BubbleZ = /*-0.84f*/6.5f;
+    ////Blubble2
+    //private float Bubble2X = /*-0.08f*/0.9f; //the same for 2 and 3 in the test scene
+    //private float Bubble2Z = /*-0.5f*/6.5f;
+    ////Blubble3
+    //private float Bubble3X = /*-0.08f*/1.2f;
+    //private float Bubble3Z = /*-0.84f*/6.5f;
+    //no more magic numbers :D
 
     private float Paper1XPosition;
     
@@ -63,10 +66,10 @@ public class WallScript : MonoBehaviour
             
             
             Bubble.SetActive(true);
-            Bubble.transform.position = Ball.transform.position + /*Vector3.forward*/ new Vector3/*(0,0,bubblePosition)*/ (bubblePosition, 0, 0); //in multiplayer scene z is not forward anymore so coordinates change to x is y
+            Bubble.transform.position = Ball.transform.position + /*Vector3.forward*/ new Vector3/*(0,0,bubblePosition)*/ (0, 0, bubblePosition);
             Debug.Log("Bubbleactive"); //inactive part when outside doesn't work yet (and also doesn't make sense since its reversed)
 
-            Paper1.transform.position = Ball.transform.position + /*Vector3.forward*/ new Vector3(bubblePosition, 0, 0);
+            Paper1.transform.position = Ball.transform.position + /*Vector3.forward*/ new Vector3(0, 0, bubblePosition);
             //else
             //{
             //    //bubble coordinates == ball y&x coordinates
@@ -97,10 +100,10 @@ public class WallScript : MonoBehaviour
 
 
                 Bubble2.SetActive(true);
-                Bubble2.transform.position = Ball2.transform.position + /*Vector3.forward*/ new Vector3(bubblePosition, 0, 0);
+                Bubble2.transform.position = Ball2.transform.position + /*Vector3.forward*/ new Vector3(0, 0, bubblePosition);
             Debug.Log("Bubbleactive");
 
-                Paper2.transform.position = Ball2.transform.position + /*Vector3.forward*/ new Vector3(bubblePosition, 0, 0);
+                Paper2.transform.position = Ball2.transform.position + /*Vector3.forward*/ new Vector3(0, 0, bubblePosition);
         }
 
         if (collision.gameObject.tag == "Ball3")
@@ -113,11 +116,11 @@ public class WallScript : MonoBehaviour
 
 
             Bubble3.SetActive(true);
-            Bubble3.transform.position = Ball3.transform.position + /*Vector3.forward*/ new Vector3(bubblePosition, 0, 0);
+            Bubble3.transform.position = Ball3.transform.position + /*Vector3.forward*/ new Vector3(0, 0, bubblePosition);
             Debug.Log("Bubbleactive");
 
             //Paper.SetActive(true);
-            Paper3.transform.position = Ball3.transform.position + /*Vector3.forward*/ new Vector3(bubblePosition, 0, 0);
+            Paper3.transform.position = Ball3.transform.position + /*Vector3.forward*/ new Vector3(0, 0, bubblePosition);
         }
 
         //------------------------------------------------------------------------
@@ -136,7 +139,8 @@ public class WallScript : MonoBehaviour
             Debug.Log("Bubbleinactive"); //inactive part when outside doesn't work yet (and also doesn't make sense since its reversed)
             Paper1.SetActive(false);
             Ball.SetActive(true);
-            Ball.transform.position = new Vector3(BubbleX, Bubble123Y, BubbleZ); //just set it far away from collider (mebi back to table)
+            //Ball.transform.position = new Vector3(BubbleX, Bubble123Y, BubbleZ); //just set it far away from collider (mebi back to table)
+            Ball.transform.position = BallBowl.transform.position + new Vector3(0, 0.5f, 0); //0.2 because height has to be different if they all respawn at once
             Ball.transform.rotation = Quaternion.identity; //to reset the rotation too cause it was weird b4
             //maybe freeze rotation until collision with hand (since the balls sometimes roll away again after respawning)
             
@@ -147,7 +151,8 @@ public class WallScript : MonoBehaviour
             Bubble2.SetActive(false);
             Paper2.SetActive(false);
             Ball2.SetActive(true);
-            Ball2.transform.position = new Vector3(Bubble2X, Bubble123Y, Bubble2Z);
+            //Ball2.transform.position = new Vector3(Bubble2X, Bubble123Y, Bubble2Z);
+            Ball2.transform.position = BallBowl.transform.position + new Vector3(0, 1f, 0);
             Ball2.transform.rotation = Quaternion.identity;
 
         }
@@ -157,7 +162,8 @@ public class WallScript : MonoBehaviour
             Bubble3.SetActive(false);
             Paper3.SetActive(false);
             Ball3.SetActive(true);
-            Ball3.transform.position = new Vector3(Bubble3X, Bubble123Y, Bubble3Z);
+            //Ball3.transform.position = new Vector3(Bubble3X, Bubble123Y, Bubble3Z);
+            Ball3.transform.position = BallBowl.transform.position + new Vector3(0, 1.5f, 0);
             Ball3.transform.rotation = Quaternion.identity;
 
             
